9f6f5a2b0eb5b4838ea97e9aa4af3a8d4b0f7577
wilson6783 <minecraft.chito555@gmail.com>
2017-06-20 11:12:19 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20170620144908) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
