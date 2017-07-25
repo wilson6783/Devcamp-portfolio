909ebc21b3cb85ecbf7e60dc3436a1a033443207
wilson6783 <minecraft.chito555@gmail.com>
2017-07-25 15:50:59 +0000

    Integrated Nested attributes for portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ puts "5 skills created"
 8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "Angular",
+    subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
