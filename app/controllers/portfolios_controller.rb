0d8c088d2a562071c9395efd982189ebd8ac8649
wilson6783 <minecraft.chito555@gmail.com>
2017-07-27 17:54:05 +0000

    Implementd form configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,8 @@ class PortfoliosController < ApplicationController
     3.times { @portfolio_item.technologies.build }
   end
   def create
-     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+     technologies_attributes: [:name]))
      respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully created!' }
