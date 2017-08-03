ac04841a228f0dc84b364926eee84c23e7c97fc8
wilson6783 <minecraft.chito555@gmail.com>
2017-08-03 14:31:36 +0000

    Refactored strong params for portfolio items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,10 @@
 class PagesController < ApplicationController
-  def home
-    @posts = Blog.all
-    @skills = Skill.all
-  end
-
-  def about
-  end
-
-  def contact
-  end
+def home
+@posts = Blog.all
+@skills = Skill.all
+end
+def about
+end
+def contact
+end
 end
